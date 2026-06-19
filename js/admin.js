4fa6025af003502a26c5ad52eee28e044a690e7c
sigit_p <35914314+sigit-p@users.noreply.github.com>
2026-06-19 20:45:32 +0700

    Add loadJobsheet function to handle job data

--- js/admin.js
+++ js/admin.js
@@ -6,6 +6,8 @@ loadKelas();
 
 kelas.onchange = loadSiswa;
 
+siswa.onchange = loadJobsheet;
+
 async function loadKelas() {
 
     let res = await fetch(API + "?action=getKelas");
@@ -53,5 +55,71 @@ async function loadSiswa() {
     });
 
     siswa.innerHTML = html;
+    
+    loadJobsheet();
+}
+
+async function loadJobsheet(){
+
+    let nis = siswa.value;
+
+    let resJob = await fetch(
+        API+"?action=getJobsheet&id_mapel=PKSM&tingkat=XI"
+    );
+
+    let jobs = await resJob.json();
+
+
+    let resNilai = await fetch(
+        API+"?action=getNilai&nis="+nis
+    );
+
+    let nilai = await resNilai.json();
+
+    let html = "";
+
+    jobs.forEach(job=>{
+
+        let n = "";
+
+        nilai.forEach(item=>{
+
+            if(item.id_job==job.id_job){
+
+                n = item.nilai;
+
+            }
+
+        });
+
+        html += `
+        <div class="card">
+
+        <h3>${job.judul_job}</h3>
+
+        <small>${job.media}</small>
+
+        <br><br>
+
+        <input
+            type="number"
+            id="${job.id_job}"
+            value="${n}"
+        >
+
+        </div>
+        `;
+
+    });
+
+    html += `
+    <button onclick="simpanNilai()">
+        💾 Simpan
+    </button>
+    `;
+
+    jobsheet.innerHTML = html;
 
 }
+
+

b10f72e6a65c6a71de8502cb020d029ede57287b
sigit_p <35914314+sigit-p@users.noreply.github.com>
2026-06-19 21:33:47 +0700

    Implement loadRekap function for student data
    Added async function to load student data and calculate average.

--- js/rekap.js
+++ js/rekap.js
@@ -1,1 +1,16 @@
 
+async function loadRekap(){
+
+  let siswa=await getSiswa();
+  let nilai=await getNilai();
+
+  //filter kelas
+
+  //loop siswa
+
+  //ambil nilai siswa
+
+  //hitung rata-rata
+
+  //buat baris tabel
+}

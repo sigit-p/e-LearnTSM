a980b2102f6d741dfe47e5899b55bd024cdcb534
sigit_p <35914314+sigit-p@users.noreply.github.com>
2026-06-20 23:14:29 +0700

    Add dashboard function to render HTML content

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function dashboard(){
+
+    document.getElementById("konten").innerHTML = `
+    <div class="card">
+
+    <h1>Dashboard</h1>
+
+    <p>
+    Selamat datang di e-Nilai Praktik Mupa
+    </p>
+
+    </div>
+    `;
+}
